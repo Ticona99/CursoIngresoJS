827daaafe9731581e80a4061f2471914d7e097b6
ticona99 <ticona235@gmail.com>
2018-07-31 12:23:25 -0300

    cammios dos tres seis

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = prompt("ingrese el número de repeticiones");
 
 
+for(var i=repeticiones; i++){
+
+	alert("Hola UTN FRA")
+}
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
+	var numero;
+	numero = prompt("ingrese un numero");
+
+	if(numero % 2 != 0){
+
+		numero--
+	}
+
+	for (var i = 1; i <= numero; i++) {
 
 
+	}
 
 
 }//FIN DE LA FUNCIÓN
